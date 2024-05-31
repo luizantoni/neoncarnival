f4189f3991bcf41fa324111bf4f6d79ae308648f
antonio azzolino <43556326+luizantoni@users.noreply.github.com>
2024-05-31 20:48:54 -0300

    neon carnival
    first version

---
+++ ShootingRangee/src/shooting-range/audio.ts
@@ -0,0 +1,22 @@
+// import { AudioSource, engine } from "@dcl/sdk/ecs"
+
+// // Create entity
+// export const sourceEntity = engine.addEntity()
+
+// // Create AudioSource component
+// AudioSource.create(sourceEntity, {
+// 	audioClipUrl: 'sounds/explosion.mp3',
+// 	playing: true,
+// })
+
+// // Define a simple function
+// function stopSound(entity: Entity) {
+// 	// fetch mutable version of audio source component
+// 	const audioSource = AudioSource.getMutable(entity)
+
+// 	// modify its playing value
+// 	audioSource.playing = false
+// }
+
+// // call function
+// stopSound(sourceEntity)
